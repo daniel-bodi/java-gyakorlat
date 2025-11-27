ccdab350ef0fb14ddd22c7822f60d1c87f0fda2d
danielbodi <bodi.daniel@hibridlevel.hu>
2025-11-27 19:16:53 +0100

    springboot test tiltas

--- src/test/java/hu/nje/java/gfekak/java/gyakorlat/JavaGyakorlatApplicationTests.java
+++ src/test/java/hu/nje/java/gfekak/java/gyakorlat/JavaGyakorlatApplicationTests.java
@@ -1,8 +1,10 @@
 package hu.nje.java.gfekak.java.gyakorlat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class JavaGyakorlatApplicationTests {
 
